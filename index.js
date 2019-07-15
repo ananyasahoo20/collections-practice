06fd9a4e341a06c790269b73e839366dccab6b81
Apple <apple@Apples-MacBook-Pro-7.local>
2019-07-15 16:36:33 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// var familyMembers = ["Pradeep", "Sasmita", "Ananya", "Shreya"]
+// familyMembers.push("Bob");
+// console.log(familyMembers);
+
+// familyMembers.pop();
+// console.log(familyMembers);
+
+// random number selector
+
+// var randomDecimal = Math.random();
+// var random = randomDecimal *20;
+// var final = Math.floor(random);
+//
+// console.log(randomDecimal);
+// console.log(random);
+// console.log(final);
+
+var candy = ['Twix', 'Sour Patch', 'M&Ms', 'Skittles', 'Reeses', 'Swedish Fish'];
+
+for (var i=0; i <= 5; i++){
+  console.log(`${candy[i]} is the best!`);
+
+
+}
